4b4b5efb8d76ad87d4a5edd257579c33b5de515f
Af17-esk <Abdullahfaiz.esketchers@gmail.com>
2022-02-11 20:07:53 +0500

    Added db

--- market_app/config.py
+++ market_app/config.py
@@ -6,5 +6,5 @@ load_dotenv()
 class Config:
     CORS_HEADERS = 'Content-Type'
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = True
