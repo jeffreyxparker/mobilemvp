b7476038fba250773fdaabecb0ee6f7ede824226
Af17-esk <Abdullahfaiz.esketchers@gmail.com>
2022-02-11 17:54:30 +0500

    Added final structure to project in running form

--- backend/market_app/config.py
+++ backend/market_app/config.py
@@ -6,5 +6,5 @@ load_dotenv()
 class Config:
     CORS_HEADERS = 'Content-Type'
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:12345678@localhost:5432/market"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
