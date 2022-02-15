cf5fed9c74acaebc8e2311902431e78e74f0a80e
Af17-esk <Abdullahfaiz.esketchers@gmail.com>
2022-02-15 16:51:32 +0500

    Added cors

--- market_app/__init__.py
+++ market_app/__init__.py
@@ -10,6 +10,5 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
     app.config['CORS_HEADERS'] = 'Content-Type'
-    CORS(app, resources={r'/api/start_survey': {"origins": "https://market-frontend-app.herokuapp.com/"},
-                         r'/api/end_survey': {"origins": "https://market-frontend-app.herokuapp.com/"}})
+    CORS(app)
     return app
